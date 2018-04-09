1629ae869b5be5e1bf6aa6d18239c4dd9bb5117f
Phyo Phyo Lwin <1041577@fairprice.sg>
2018-04-09 18:05:56 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) =>{
+    //Can be used to call db to authenticate the user or log the data
+    //It will not move on until next() is called in middle ware.
+    //If next() never gets called, the route will not be rendered.
+    let now = new Date().toString();
+    fs.appendFile('server.log', `${now}: ${req.method} ${req.url} \n`,(err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+
+}); //app.use is registering middleware
+
+// app.use((req, res, next) =>{// This will show the maintenance page and can not view any other pages.
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));//Telling express where to use the static pages, Setting public static pages which everyone can accessf
+
+hbs.registerHelper('getCurrentYear',()=>{//hbs template 
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{//Capitalization helper
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+    res.render('home.hbs',{
+        welcomeMessage: 'Welcome to the home page!',
+        pageTitle: 'Home Page'
+        
+    })
+        
+    // res.send('<h1>Hello Express</h1>');
+
+    // res.send({
+    //     name: 'Phyo Phyo',
+    //     likes: [
+    //         'Reading',
+    //         'Biking'
+    //     ]
+    // });
+});
+
+app.get('/about', (req, res)=>{
+    // res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+})
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        bad: 'bad request',
+        errorMsg: [
+            'lala',
+            'dada'
+        ]
+    });
+})
+
+app.listen(3000,() =>{
+    console.log('Server is up on port 3000');
+});
